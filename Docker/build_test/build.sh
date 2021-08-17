9b4ddb3dae5e5691770f7596c457daf0ca3769ab
Ignacio Martinez <martinezig@google.com>
2021-08-17 14:43:29 +0000

    adds `devtools::test()` to the docker test

--- Docker/build_test/build.sh
+++ Docker/build_test/build.sh
@@ -7,6 +7,6 @@ docker run --rm \
   -v $(pwd)/../..:/tmp/bsynth \
   -w /tmp/working_dir \
   bsynth \
-  R -e "devtools::build(pkg = '/tmp/bsynth', path = '/tmp/working_dir, binary = TRUE')"
+  R -e "devtools::test(pkg = '/tmp/bsynth'); devtools::build(pkg = '/tmp/bsynth', path = '/tmp/working_dir, binary = TRUE')"
 
 
